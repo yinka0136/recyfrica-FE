377cb8a79fb64e439ec5dbf72d596334c5d5292d
yinka0136 <yinka0136@gmail.com>
2021-05-18 14:42:28 +0100

    commit

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -12,7 +12,7 @@ export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.type();
+      this.do()
       AOS.init({
         offset: 400,
         duration: 500,
@@ -22,52 +22,48 @@ export class LandingComponent implements OnInit {
       });
     }
   }
-  type() {
-    const typedTextSpan: any = document.querySelector('.typed-text');
-    const cursorSpan: any = document.querySelector('.cursor');
+  do() {
+    const typedTextSpan:any = document.querySelector(".typed-text");
+const cursorSpan:any = document.querySelector(".cursor");
 
-    const textArray = ['recycling', 'upcycling', 'saving the environment'];
-    const typingDelay = 200;
-    const erasingDelay = 100;
-    const newTextDelay = 2000; // Delay between current and next text
-    let textArrayIndex = 0;
-    let charIndex = 0;
+const textArray = ['recycling', 'upcycling', 'saving the environment'];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+type()
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
 
-    function type() {
-      if (charIndex < textArray[textArrayIndex].length) {
-        if (!cursorSpan.classList.contains('typing'))
-          cursorSpan.classList.add('typing');
-        typedTextSpan.textContent +=
-          textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-      } else {
-        cursorSpan.classList.remove('typing');
-        setTimeout(erase, newTextDelay);
-      }
-    }
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
 
-    function erase() {
-      if (charIndex > 0) {
-        if (!cursorSpan.classList.contains('typing'))
-          cursorSpan.classList.add('typing');
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(
-          0,
-          charIndex - 1
-        );
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-      } else {
-        cursorSpan.classList.remove('typing');
-        textArrayIndex++;
-        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-        setTimeout(type, typingDelay + 1100);
-      }
-    }
-
-    document.addEventListener('DOMContentLoaded', function () {
-      // On DOM Load initiate the effect
-      if (textArray.length) setTimeout(type, newTextDelay + 250);
-    });
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+   
   }
 }
