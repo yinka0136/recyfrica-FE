c38519f1a1a2294da895b33d94cdf4abf18748e5
yinka0136 <yinka0136@gmail.com>
2021-05-18 16:55:36 +0100

    commit

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import * as AOS from 'aos';
 @Component({
   selector: 'app-landing',
@@ -8,11 +9,15 @@ import * as AOS from 'aos';
 })
 export class LandingComponent implements OnInit {
   year: any = new Date().getFullYear();
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private title: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.do()
+      this.do();
       AOS.init({
         offset: 400,
         duration: 500,
@@ -23,47 +28,51 @@ export class LandingComponent implements OnInit {
     }
   }
   do() {
-    const typedTextSpan:any = document.querySelector(".typed-text");
-const cursorSpan:any = document.querySelector(".cursor");
+    const typedTextSpan: any = document.querySelector('.typed-text');
+    const cursorSpan: any = document.querySelector('.cursor');
 
-const textArray = ['recycling', 'upcycling', 'saving the environment'];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-type()
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
+    const textArray = ['recycling', 'upcycling', 'saving the environment'];
+    const typingDelay = 200;
+    const erasingDelay = 100;
+    const newTextDelay = 2000; // Delay between current and next text
+    let textArrayIndex = 0;
+    let charIndex = 0;
+    type();
+    function type() {
+      if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains('typing'))
+          cursorSpan.classList.add('typing');
+        typedTextSpan.textContent +=
+          textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+      } else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
+      }
+    }
 
-function erase() {
-  if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
+    function erase() {
+      if (charIndex > 0) {
+        if (!cursorSpan.classList.contains('typing'))
+          cursorSpan.classList.add('typing');
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(
+          0,
+          charIndex - 1
+        );
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+      } else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+      }
+    }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
-   
+    document.addEventListener('DOMContentLoaded', function () {
+      // On DOM Load initiate the effect
+      if (textArray.length) setTimeout(type, newTextDelay + 250);
+    });
   }
 }
