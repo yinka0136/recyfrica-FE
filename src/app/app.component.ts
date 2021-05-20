6646e97ac590dde3c3927b9f170547f640d9bf40
yinka0136 <yinka0136@gmail.com>
2021-05-20 18:19:13 +0100

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,23 @@ export class AppComponent implements OnInit {
   constructor(private metaService: Meta, private titleService: Title) {}
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
-    // this.metaService.addTags([
-    //   {
-    //     name: 'keywords',
-    //     content:
-    //       'Collection point Locator,dedicated,recycling,rewarding,experience,climate,action,rewarded,community,.from app.component',
-    //   },
-    //   {
-    //     name: 'description',
-    //     content:
-    //       'Recyfrica is a social and micro rewards platform to create an awesome experience for you to help curb the growing waste menace across urban African cities.',
-    //   },
-    //   { name: 'robots', content: 'index, follow' },
-    // ]);
+    this.metaService.addTags([
+      {
+        name: 'keywords',
+        content:
+          'Collection point Locator,dedicated,recycling,rewarding,experience,climate,action,rewarded,community,from app.component',
+      },
+      {
+        name: 'description',
+        content:
+          'Recyfrica is a social and micro rewards platform to create an awesome experience for you to help curb the growing waste menace across urban African cities.',
+      },
+      {
+        name: 'keywords',
+        content:
+          'plastic pollution, waste management, zero waste, sustainability matters, climate change, environment , sustainability, circular economy, recycling, community, climate crisis, plastic recycling',
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
   }
 }
