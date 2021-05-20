28cc1b42236fe46d63ebd02d1f1b93e63a7130ea
yinka0136 <yinka0136@gmail.com>
2021-05-20 11:58:44 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'recyfrica';
+  title = 'Recyfrica | Sustainable Waste Management Service.';
   constructor(private metaService: Meta, private titleService: Title) {}
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
