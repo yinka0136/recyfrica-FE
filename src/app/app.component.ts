ae8416efcd62b61b39244153b9948532f9f49b63
yinka0136 <yinka0136@gmail.com>
2021-05-20 18:01:18 +0100

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,18 @@ export class AppComponent implements OnInit {
   constructor(private metaService: Meta, private titleService: Title) {}
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
-    this.metaService.addTags([
-      {
-        name: 'keywords',
-        content:
-          'Collection point Locator,dedicated,recycling,rewarding,experience,climate,action,rewarded,community,.from app.component',
-      },
-      {
-        name: 'description',
-        content:
-          'Recyfrica is a social and micro rewards platform to create an awesome experience for you to help curb the growing waste menace across urban African cities.',
-      },
-      { name: 'robots', content: 'index, follow' },
-    ]);
+    // this.metaService.addTags([
+    //   {
+    //     name: 'keywords',
+    //     content:
+    //       'Collection point Locator,dedicated,recycling,rewarding,experience,climate,action,rewarded,community,.from app.component',
+    //   },
+    //   {
+    //     name: 'description',
+    //     content:
+    //       'Recyfrica is a social and micro rewards platform to create an awesome experience for you to help curb the growing waste menace across urban African cities.',
+    //   },
+    //   { name: 'robots', content: 'index, follow' },
+    // ]);
   }
 }
